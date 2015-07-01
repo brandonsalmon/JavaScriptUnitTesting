1c961d07baaff41303d0a0cb3e16a7ed8f1d7f26
Brandon Salmon <brandon.j.salmon@gmail.com>
2015-07-01 14:58:10 -0500

    Pager

---
+++ js/app/Pager.js
@@ -0,0 +1,16 @@
+﻿angular.module('cap').controller('capPagerController', ['$scope', function ($scope) {
+
+    $scope.capPagerStartFrom = 0;
+    $scope.capPagerPageSize = 10;
+
+}]).directive('capPager', function () {
+    return {
+        scope: {
+            capPagerItems: '=',
+            capPagerPageSize: '=',
+            capPagerStartFrom: '='
+        },
+        controller: 'capPagerController',
+        templateUrl: '/js/app/Pager.html'
+    };
+});
